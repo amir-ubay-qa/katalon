73ba2c5132d16c1b80547d81299e2ad82e696bd4
ThinkPad T14 <ThinkPad T14@DESKTOP-PJ4NMRC>
2024-08-13 13:20:33 +0700

    Add report and ai stuido generated testcases and cli

---
+++ Scripts/ultimate qa - course search is visible/Script1723529841254.groovy
@@ -0,0 +1,29 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+/* Write a Katalon Studio test case to perform the following steps.
+ * 1. Open browser to the URL stored in https://courses.ultimateqa.com/collections
+ * 2. Verify component search is visible
+ * */
+// Open browser to the URL stored in https://courses.ultimateqa.com/collections
+WebUI.openBrowser('')
+WebUI.navigateToUrl('https://courses.ultimateqa.com/collections')
+
+// Verify component search is visible
+WebUI.verifyElementVisible(findTestObject('Page_Courses - Ultimate QA/component_search'))
---
+++ Scripts/ultimate qa - go to service page/Script1723529254348.groovy
@@ -0,0 +1,37 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+/* Write a Katalon Studio test case to perform the following steps.
+ * 1. Open browser to the URL stored in https://ultimateqa.com/
+ * 2. Find menu service
+ * 3. Click menu service
+ * 4. Verify page service is opened
+ * */
+// Open browser to the URL stored in https://ultimateqa.com/
+WebUI.openBrowser('')
+WebUI.navigateToUrl('https://ultimateqa.com/')
+
+// Find menu service
+WebUI.click(findTestObject('Page_UltimateQA/menu_Service'))
+
+// Click menu service
+WebUI.click(findTestObject('Page_UltimateQA/menu_Service'))
+
+// Verify page service is opened
+WebUI.verifyElementVisible(findTestObject('Page_UltimateQA/page_Service'))
